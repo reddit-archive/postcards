d73a060ec176104b0900250e58aae2e9c9a3042d
Neil Williams <neil@reddit.com>
2011-09-25 22:02:00 -0700

    It does stuff. It's ugly.

--- postcards.py
+++ postcards.py
@@ -1,17 +1,100 @@
-from flask import Flask
+import boto
+import hashlib
+import base64
+import os
+import datetime
+from flask import Flask, render_template, redirect, request, flash
 from flaskext.sqlalchemy import SQLAlchemy
+from flaskext import wtf
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///postcards.db'
+app.secret_key = os.urandom(24)
 db = SQLAlchemy(app)
+s3 = boto.connect_s3()
+bucket = s3.get_bucket('postcards.reddit.com')
 
 class Postcard(db.Model):
+    __tablename__ = 'postcards'
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(20))
     country = db.Column(db.String(3))
     date = db.Column(db.Date)
     latitude = db.Column(db.Numeric)
     longitude = db.Column(db.Numeric)
+    front = db.Column(db.String)
+    back = db.Column(db.String)
+    tags = db.relationship("Tag")
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    postcard_id = db.Column(db.Integer, db.ForeignKey('postcards.id'))
+    tag = db.Column(db.String)
+
+class PostcardForm(wtf.Form):
+    username = wtf.TextField(
+        'username',
+        validators=[
+            wtf.Length(max=20),
+            wtf.Required()
+        ]
+    )
+
+    origin = wtf.TextField('origin', validators=[wtf.Required()])
+    date = wtf.DateField('date of postmark',
+                         format='%m/%d/%Y',
+                         default=datetime.date(2010, 01, 01))
+    origin_country = wtf.HiddenField()
+    origin_latitude = wtf.DecimalField()
+    origin_longitude = wtf.DecimalField()
+    front = wtf.TextField('front of card')
+    back = wtf.TextField('back of card')
+    tags = wtf.TextField('tags (comma-delimited)')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/postcard/new', methods=['GET', 'POST'])
+def new_postcard_form():
+    form = PostcardForm(request.form)
+    if request.method == 'POST' and form.validate():
+        postcard = Postcard()
+        postcard.user = form.username.data
+        postcard.date = form.date.data
+        postcard.country = form.origin_country.data
+        postcard.latitude = form.origin_latitude.data
+        postcard.longitude = form.origin_longitude.data
+        postcard.front = form.front.data
+        postcard.back = form.back.data
+        db.session.add(postcard)
+
+        for tag in (x.strip() for x in form.tags.data.split(',')):
+            t = Tag()
+            t.tag = tag
+            postcard.tags.append(t)
+
+        db.session.commit()
+
+        flash('postcard added!')
+        return redirect('/postcard/new')
+    return render_template('postcard_new.html', form=form)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    data = base64.b64decode(request.data)
+    digest = hashlib.sha1(data).digest()
+    filename = base64.urlsafe_b64encode(digest[:8]).rstrip('=') + '.jpg'
+    key = bucket.new_key(filename)
+    key.set_contents_from_string(
+        data,
+        headers={'Content-Type': 'image/jpeg'},
+        policy='public-read',
+    )
+    return filename
 
 if __name__ == "__main__":
-    app.run()
+    db.create_all()
+    app.run(debug=True)
---
+++ static/postcard.js
@@ -0,0 +1,132 @@
+$(function() {
+    var geocoder = new google.maps.Geocoder()
+    var geocodeResult = undefined
+    var filesUploaded = 0
+
+    function checkFormValid() {
+        var isValid = (
+            $("#origin").val() == geocodeResult &&
+            $("#username").val() != '' &&
+            $("#date").val() != '' &&
+            filesUploaded == 2
+        )
+
+        if (isValid)
+            $("#submit").removeAttr('disabled')
+        else
+            $("#submit").attr('disabled', '')
+    }
+
+    $("#origin")
+        .autocomplete({
+            source: function(request, response) {
+                geocoder.geocode({
+                    address: request.term
+                }, function(results, status) {
+                    if (status != 'OK')
+                        return
+
+                    var autocompletion = $.map(results, function(res, i) {
+                        return {
+                            value: res.formatted_address,
+                            result: res
+                        }
+                    })
+
+                    response(autocompletion)
+                })
+            },
+
+            select: function(event, ui) {
+                var result = ui.item.result
+
+                $.each(result.address_components, function(i, c) {
+                    if ($.inArray("country", c.types) >= 0) {
+                        $("#origin_country").val(c.short_name)
+                        return false
+                    }
+                })
+
+                var location = result.geometry.location
+                $("#origin_latitude").val(location.lat())
+                $("#origin_longitude").val(location.lng())
+                $("#origin_country").val()
+
+                geocodeResult = ui.item.value
+                checkFormValid()
+            }
+        })
+
+    // set up the datepicker
+    $("#date").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        onSelect: function(dateText, instance) {
+            checkFormValid()
+        }
+    })
+
+    $("input[type=file]").change(function() {
+        if (this.files.length != 1)
+            return
+
+        var reader = new FileReader()
+        var xhr = new XMLHttpRequest()
+        var progress = null
+        var fileInput = $(this)
+        var cell = fileInput.parent()
+
+        fileInput.hide()
+
+        xhr.upload.addEventListener("progress", function(e) {
+            if (!e.lengthComputable)
+                return
+
+            if (progress === null)
+                progress = $("<progress>")
+                                .attr({
+                                    max: e.total,
+                                    value: e.loaded
+                                })
+                                .appendTo(cell)
+            else
+                progress.val(e.loaded)
+        }, false)
+
+        xhr.upload.addEventListener("load", function(e) {
+            cell
+                .empty()
+                .text('saving to s3...')
+        }, false)
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState != 4)
+                return
+
+            cell.text('uploaded')
+
+            $('<input>')
+                .attr({
+                    name: fileInput.attr('name'),
+                    type: 'hidden',
+                    value: xhr.responseText
+                })
+                .appendTo(cell)
+
+            filesUploaded += 1
+            checkFormValid()
+        }
+
+        reader.onload = function(evt) {
+            xhr.send(btoa(evt.target.result))
+        }
+
+        xhr.open('POST', '/upload')
+        reader.readAsBinaryString(this.files[0])
+    })
+
+    $("input")
+        .keyup(checkFormValid)
+        .change(checkFormValid)
+    checkFormValid()
+})
