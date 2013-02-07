0f6a2e86583b7b475e9d49c478ba9b8cf86fd799
Neil Williams <neil@reddit.com>
2013-02-06 16:17:38 -0800

    Add middleware to set wsgi.url_scheme based on X-Scheme.

--- postcards/__init__.py
+++ postcards/__init__.py
@@ -10,5 +10,19 @@ app = Flask(__name__)
 app.config.from_envvar('POSTCARD_SETTINGS')
 app.secret_key = os.urandom(24)
 
+# let us handle ssl better
+# http://flask.pocoo.org/snippets/35/
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get("HTTP_X_SCHEME")
+        if scheme:
+            environ["wsgi.url_scheme"] = scheme
+        return self.app(environ, start_response)
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
 # make sure the views get registered
 import postcards.views
